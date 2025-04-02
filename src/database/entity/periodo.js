de63cd033968851f22120333544b9cf20a645390
Leo-Slv <leonardo1692004@gmail.com>
2025-04-02 20:07:07 -0300

    Criação entidade periodo

---
+++ src/database/entity/periodo.js
@@ -0,0 +1,14 @@
+class periodo {
+
+  constructor(id, dia_id, horario_id, disciplina_id, docente_id, semestre_id, ambiente_id) {
+
+    this.id = id;
+    this.dia_id = dia_id;
+    this.horario_id = horario_id;
+    this.disciplina_id = disciplina_id;
+    this.docente_id = docente_id;
+    this.semestre_id = semestre_id;
+    this.ambiente_id = ambiente_id;
+    
+  }
+}
