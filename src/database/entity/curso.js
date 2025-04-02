7ea30e8b5ce8af6856e47e906fac13a187ed3eb9
Leo-Slv <leonardo1692004@gmail.com>
2025-04-02 16:09:35 -0300

    Criação método updateCurso | Entidade curso

--- src/database/entity/curso.js
+++ src/database/entity/curso.js
@@ -33,7 +33,7 @@ class curso {
     try{
 
      //Buscar todos os cursos no banco (ainda a ser implementado)
-
+     
      const allEntitys = []; // Aqui será preenchido com os dados do banco
 
       return allEntitys
@@ -43,13 +43,24 @@ class curso {
        return { status: "erro", mensagem: erro.message };
 
     }
-
-
   }
 
   updateCurso(id) {
-    
+    try{
+      //Buscar curso no banco pelo ID (ainda a ser implementado)
 
+      const curso = new curso(); // Aqui será preenchido com os dados do banco
+
+      if (curso.name != null) {
+          //Atualizar o curso no banco (ainda a ser implementado)
+      }
+      return { status: "sucesso", mensagem: "Curso atualizado!" };
+
+    }catch (erro){
+
+       return { status: "erro", mensagem: erro.message };
+      
+    }
   }
 
   deleteCurso(id) {
