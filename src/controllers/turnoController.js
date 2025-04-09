ff634668053ad2fb9e56cc8b8f302b2e4365f0ee
FelipePacheco30 <felipeferreirapacheco30@gmail.com>
2025-04-08 21:24:02 -0300

    Criação da função requestDeleteTurno

--- src/controllers/turnoController.js
+++ src/controllers/turnoController.js
@@ -1,45 +1,62 @@
 const turno = require('../database/entity/turno');
 const createTurnoDto = require('../database/entity/dto/createTurnoDto');
-const defaultStrings = require('../utils/firstLetterUppercase')
+const defaultStrings = require('../utils/firstLetterUppercase');
 
 async function requestNewTurno(req, res) {
+  const defaultNomeString = defaultStrings.firstLetterUppercase(req.body.nome);
 
-const defaultNomeString = defaultStrings.firstLetterUppercase(req.body.nome);
-
-const newTurnoDto = new createTurnoDto( defaultNomeString );
+  const newTurnoDto = new createTurnoDto(defaultNomeString);
 
   try {
-    const newTurno = new turno(null, newTurnoDto.nome)
+    const newTurno = new turno(null, newTurnoDto.nome);
 
-    const createTurno = await newTurno.createTurno( newTurno );
+    const createTurno = await newTurno.createTurno(newTurno);
     res.status(createTurno.status).json(createTurno);
   } catch (error) {
-    console.error('Erro ao criar turno:', error);
-    res.status(500).json({ erro: 'Erro interno ao criar turno' });
+    console.error("Erro ao criar turno:", error);
+    res.status(500).json({ erro: "Erro interno ao criar turno" });
   }
 }
 
 async function listTurnos(req, res) {
-    try {
-      const turnos = await turno.getAllTurno();
-      res.status(200).json(turnos);
-    } catch (error) {
-      console.error('Erro ao listar turnos:', error);
-      res.status(500).json({ erro: 'Erro interno ao buscar turnos' });
-    }
+  try {
+    const turnos = await turnos.getAllTurno();
+    res.status(200).json(turnos);
+  } catch (error) {
+    console.error("Erro ao listar turnos:", error);
+    res.status(500).json({ erro: "Erro interno ao buscar turnos" });
   }
+}
 
-  async function requestAlterTurno(req, res) {
+async function requestAlterTurno(req, res) {
+  const defaultNomeString = defaultStrings.firstLetterUppercase(req.body.nome);
 
-    const defaultNomeString = defaultStrings.firstLetterUppercase(req.body.nome);
-  
-    const alterTurno = new turno( req.body.id, defaultNomeString );
-    
-      try {
-        const updateTurno = await alterTurno.updateTurno( alterTurno );
-        res.status(updateTurno.status).json(updateTurno);
-      } catch (error) {
-        console.error('Erro ao atualizar turno:', error);
-        res.status(500).json({ erro: 'Erro interno ao atualizar turno' });
-      }
-    }
+  const alterTurno = new turno(req.body.id, defaultNomeString);
+
+  try {
+    const updateTurno = await alterTurno.updateTurno(alterTurno);
+    res.status(updateTurno.status).json(updateTurno);
+  } catch (error) {
+    console.error("Erro ao atualizar turno:", error);
+    res.status(500).json({ erro: "Erro interno ao atualizar turno" });
+  }
+}
+
+async function requestDeleteTurno(req, res) {
+  const id = req.body.id;
+
+  try {
+    const deleteTurno = await turno.deleteTurno(id);
+    res.status(deleteTurno.status).json(deleteTurno);
+  } catch (error) {
+    console.error("Erro ao atualizar turno:", error);
+    res.status(500).json({ erro: "Erro interno ao atualizar turno" });
+  }
+}
+
+module.exports = {
+  listTurnos,
+  requestNewTurno,
+  requestAlterTurno,
+  requestDeleteTurno,
+};
