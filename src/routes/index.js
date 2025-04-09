bcd4ab376cd0f0c8d5ada556ae8e829a1f0f4c8f
FelipePacheco30 <felipeferreirapacheco30@gmail.com>
2025-04-08 21:37:33 -0300

    Criação do arquivo turnoRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const healthRoutes = require("./healthRoutes");
 const cursosRoutes = require("./cursoRoutes");
+const turnosRoutes = require("./turnoRoutes");
+
 
 // Adicionamos a rota de verificação de saúde no endpoint "/health"
 router.use("/health", healthRoutes);
 router.use("/cursos", cursosRoutes);
+router.use("/turnos", turnosRoutes);
 
 module.exports = router;
 
