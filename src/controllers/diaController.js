86885caeabd916a426595a72b72b7cf2b6ec081d
TIALICIA <168659494+TIALICIA@users.noreply.github.com>
2025-04-09 19:29:52 -0300

    Create diaController.js
    Além da função requestNewDia

---
+++ src/controllers/diaController.js
@@ -0,0 +1,17 @@
+const dia = require('../database/entity/dia');
+const createDiaDto = require('../database/entity/dto/createDiaDto');
+const defaultStrings = require('../utils/firstLetterUppercase')
+
+async function requestNewDia(req, res) {
+  const defaultNomeString = defaultStrings.firstLetterUppercase(req.body.nome);
+  const newDiaDto = new createDiaDto(defaultNomeString);
+
+  try {
+    const newDia = new dia(null, newDiaDto.nome)
+    const createDia = await newDia.createDia(newDia);
+    res.status(createDia.status).json(createDia);
+  } catch (error) {
+    console.error('Erro ao criar dia:', error);
+    res.status(500).json({ erro: 'Erro interno ao criar dia' });
+  }
+}
