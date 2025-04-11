c07feae7ae22543da33709ccb48fc575b826fde9
ArthurFacchinetti <arthur.peixoto@fatec.sp.gov.br>
2025-04-11 19:47:24 -0300

    Criação da função requestAlterAmbiente

--- src/controllers/ambienteController.js
+++ src/controllers/ambienteController.js
@@ -24,4 +24,17 @@ async function listAmbientes(req, res) {
     console.error('Erro ao listar ambientes:', error);
     res.status(500).json({ erro: 'Erro interno ao buscar ambientes' });
   }
+  
+}
+async function requestAlterAmbiente(req, res) {
+  const defaultNomeString = defaultStrings.firstLetterUppercase(req.body.nome);
+  const alterAmbiente = new ambiente(req.body.id, defaultNomeString);
+
+  try {
+    const updateAmbiente = await alterAmbiente.updateAmbiente(alterAmbiente);
+    res.status(updateAmbiente.status).json(updateAmbiente);
+  } catch (error) {
+    console.error('Erro ao atualizar ambiente:', error);
+    res.status(500).json({ erro: 'Erro interno ao atualizar ambiente' });
+  }
 }
