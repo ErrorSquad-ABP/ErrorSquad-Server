86586dc2f7c2adba67e2dbef07a4c82d18614ea6
PromptdComando <ceges121106@gmail.com>
2025-04-25 22:00:21 -0300

    Lógica de leitura do csv, seguindo o fluxo de transformação em um objeto e inserção de id = null - turno

--- src/database/migrations/batches/insertBatchQuery.js
+++ src/database/migrations/batches/insertBatchQuery.js
@@ -1,25 +1,25 @@
 const bigquery = require('../../../lib/bigquery');
 
 async function insertBatch(tableName, columns, records) {
- 
+
   if (!records || records.length === 0) return;
-  
+
   // Preparação dos valores para inserção em lote
-  const values = records.map(record => {
-    const vals = columns.map(col => {
-      const val = record[col];
-      return typeof val === 'string' ? `'${val.replace(/'/g, "''")}'` : val;
-    });
-    return `(${vals.join(', ')})`;
+  const values = records.map((record, index) => {
+    return `STRUCT(${index + 1} AS seq_num, '${record.nome.replace(/'/g, "''")}' AS nome)`;
   }).join(', ');
-  
+console.log(columns.join(', '))
   const query = `
-    INSERT INTO sitefatecdsm-01-2025.SiteFatecDSM.${tableName} (${columns.join(', ')})
-    VALUES ${values};
+    INSERT INTO sitefatecdsm-01-2025.SiteFatecDSM.${tableName} (id, ${columns.join(', ')})
+    SELECT
+      COALESCE((SELECT MAX(id) FROM sitefatecdsm-01-2025.SiteFatecDSM.${tableName}), 0) + seq_num,
+       ${columns.map(col => col).join(', ')}
+    FROM UNNEST([${values}]);
   `;
 
+
   const options = { query, useLegacySql: false };
-  
+
   try {
     await bigquery.query(options);
     console.log(`Inseridos ${records.length} registros em ${tableName}`);
