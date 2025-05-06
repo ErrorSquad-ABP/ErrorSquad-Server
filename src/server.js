85e3465d6c5e5e2738da6a1ee34e78561c90dc31
Caiuuutecnologico <caiozinhoaraujo061206@gmail.com>
2025-05-06 15:07:35 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 require("dotenv").config(); // Carrega variáveis de ambiente
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT;
+const corsOptions = {
+    origin: 'https://errorsquad-server.onrender.com', // Substitua pela origem permitida
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 
 // Importa rotas
 const routes = require("./routes");
