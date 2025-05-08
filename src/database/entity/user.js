f0bf827e2cf6e077c37b94fdba2e60e2cf7deaef
Caiuuutecnologico <caiozinhoaraujo061206@gmail.com>
2025-05-08 13:51:00 -0300

    Update user.js
    Reparo em tratamento de erro

--- src/database/entity/user.js
+++ src/database/entity/user.js
@@ -14,30 +14,33 @@ class user {
   static async validateLogin(email, senha) {
     try {
       const userInfos = await userQuery.login(email);
-      const tokenInfos = ({id:userInfos.id, nome:userInfos.nome})
+
 
       if (!userInfos) {
-        return { status: 404, message: 'Email incorreto.' };
+        return { status: 404, message: 'Credenciais incorretas.' };
       }
 
       // Comparar a senha fornecida com o hash armazenado
-      const isPasswordValid = await bcrypt.compare(senha, userInfos.hashed_password);
+      if (userInfos) {
+        const tokenInfos = ({ id: userInfos.id, nome: userInfos.nome })
+        const isPasswordValid = await bcrypt.compare(senha, userInfos.hashed_password);
 
-      if (isPasswordValid) {
-        //Geração de token jwt
-        const token = generateToken(tokenInfos);
+        if (isPasswordValid) {
+          //Geração de token jwt
+          const token = generateToken(tokenInfos);
 
-        return {
-          status: 200,
-          message: 'Login realizado com sucesso!',
-          data: {
-            id: userInfos.id,
-            nome: userInfos.nome,
-            token,
-          },
-        };
-      } else {
-        return { status: 400, message: 'Senha incorreta.' };
+          return {
+            status: 200,
+            message: 'Login realizado com sucesso!',
+            data: {
+              id: userInfos.id,
+              nome: userInfos.nome,
+              token,
+            },
+          };
+        } else {
+          return { status: 404, message: 'Credenciais incorretas.' };
+        }
       }
     } catch (error) {
       console.error('Erro ao processar login:', error);
