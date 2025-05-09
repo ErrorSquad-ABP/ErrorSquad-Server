2b9ae70b94b7441c1dff93295398a4ba5570dd22
FelipePacheco30 <felipeferreirapacheco30@gmail.com>
2025-05-09 20:16:03 -0300

    Select da gradeQuery searchAllInfos com curso e turno

--- src/database/migrations/gradeQuery.js
+++ src/database/migrations/gradeQuery.js
@@ -6,6 +6,8 @@ async function searchAllInfos() {
     `SELECT 
   (SELECT ARRAY_AGG(STRUCT(id, nome)) FROM \`sitefatecdsm-01-2025.SiteFatecDSM.dia\`) AS dias,
   (SELECT ARRAY_AGG(STRUCT(id, hr_inicio, hr_fim)) FROM \`sitefatecdsm-01-2025.SiteFatecDSM.horario\`) AS horarios,
+  (SELECT ARRAY_AGG(STRUCT(id, nome, sigla)) FROM \`sitefatecdsm-01-2025.SiteFatecDSM.curso\`) AS cursos,
+  (SELECT ARRAY_AGG(STRUCT(id, nome)) FROM \`sitefatecdsm-01-2025.SiteFatecDSM.turno\`) AS turnos,
   (SELECT ARRAY_AGG(STRUCT(id, id_dia, id_horario, id_disciplina, id_docente_disciplina, id_cronograma_semestre, id_ambiente)) FROM \`sitefatecdsm-01-2025.SiteFatecDSM.periodo\`) AS periodos;
 `;
 
