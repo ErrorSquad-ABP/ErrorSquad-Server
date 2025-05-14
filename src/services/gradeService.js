926dbf7e1ce07efbee3a1efee7f9c47cd740ffb6
ArthurFacchinetti <arthur.peixoto@fatec.sp.gov.br>
2025-05-14 02:56:46 -0300

    endpoint put para alterar periodo arrastando no front

--- src/services/gradeService.js
+++ src/services/gradeService.js
@@ -1,4 +1,5 @@
 const gradeQuery = require ('../database/migrations/gradeQuery')
+const hasNullValues = require('../utils/hasNullValues')
 
 async function getAllInfosToGrade() {
 
@@ -12,6 +13,28 @@ async function getAllInfosToGrade() {
   }
 }
 
+async function swapPeriodosInGrade(card_1, card_2) {
+
+  try {
+  // Verificar card_1 e card_2
+  if (hasNullValues(card_1)) {
+      console.log('card_1 possui campos nulos.');
+  }
+
+  if (hasNullValues(card_2)) {
+      console.log('card_2 possui campos nulos.');
+  }
+
+    return await gradeQuery.swapPeriodos(card_1.id, card_1.id_dia, card_1.id_horario, card_2.id, card_2.id_dia, card_2.id_horario);
+   
+  } catch (error) {
+    console.error('Erro ao buscar informações:', error);
+    res.status(500).json({ erro: 'Erro interno ao buscar cursos' });
+  }
+}
+
+
 module.exports = {
-  getAllInfosToGrade
+  getAllInfosToGrade,
+  swapPeriodosInGrade
 };
